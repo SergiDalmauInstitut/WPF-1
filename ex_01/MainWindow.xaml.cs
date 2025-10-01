5d3849a1f15cddb68a8d1bb4584e593c2cd51497
SergiDalmauInsti <sdalmau.dam@institutcampalans.net>
2025-10-01 20:05:20 +0200

    Third commit

--- ex_01/MainWindow.xaml.cs
+++ ex_01/MainWindow.xaml.cs
@@ -26,6 +26,10 @@ namespace ex_01
 
             WrapPanel wpanel = new WrapPanel();
 
+            StackPanel spanel1 = new StackPanel();
+            StackPanel spanel2 = new StackPanel();
+            StackPanel spanel3 = new StackPanel();
+
             this.Background = Brushes.LightGray;
 
             this.Content = wpanel;
@@ -45,39 +49,82 @@ namespace ex_01
             button3.Background = Brushes.AliceBlue;
             button3.BorderThickness = new Thickness(0);
 
-            wpanel.Children.Add(button1);
-            wpanel.Children.Add(button2);
-            wpanel.Children.Add(button3);
+            Button button4 = new Button();
+            button4.Margin = new Thickness(10.0);
+            button4.Background = Brushes.AliceBlue;
+            button4.BorderThickness = new Thickness(0);
+
+            Button button5 = new Button();
+            button5.Margin = new Thickness(10.0);
+            button5.Background = Brushes.AliceBlue;
+            button5.BorderThickness = new Thickness(0);
+            
+            Button button6 = new Button();
+            button6.Margin = new Thickness(10.0);
+            button6.Background = Brushes.AliceBlue;
+            button6.BorderThickness = new Thickness(0);
+
+            Button button7 = new Button();
+            button7.Margin = new Thickness(10.0);
+            button7.Background = Brushes.AliceBlue;
+            button7.BorderThickness = new Thickness(0);
+
+            Button button8 = new Button();
+            button8.Margin = new Thickness(10.0);
+            button8.Background = Brushes.AliceBlue;
+            button8.BorderThickness = new Thickness(0);
+            
+            Button button9 = new Button();
+            button9.Margin = new Thickness(10.0);
+            button9.Background = Brushes.AliceBlue;
+            button9.BorderThickness = new Thickness(0);
+
+            wpanel.Children.Add(spanel1);
+            wpanel.Children.Add(spanel2);
+            wpanel.Children.Add(spanel3);
+
+            spanel1.Children.Add(button1);
+            spanel1.Children.Add(button4);
+            spanel1.Children.Add(button7);
+            spanel2.Children.Add(button2);
+            spanel2.Children.Add(button5);
+            spanel2.Children.Add(button8);
+            spanel3.Children.Add(button3);
+            spanel3.Children.Add(button6);
+            spanel3.Children.Add(button9);
 
             TextBlock text1 = new TextBlock();
-            text1.Text = "Hola";
-            text1.FontWeight = FontWeights.Bold;
-            text1.FontSize = 30;
-            text1.Foreground = Brushes.Cyan;
+            text1.Text = "X";
 
-            TextBlock text2 = new TextBlock();
-            text2.Text = "Bon";
-            text2.FontWeight = FontWeights.Bold;
-            text2.FontSize = 30;
-            text2.Foreground = Brushes.Magenta;
-
-            TextBlock text3 = new TextBlock();
-            text3.Text = "Dia";
-            text3.FontWeight = FontWeights.Bold;
-            text3.FontSize = 30;
-            text3.Foreground = Brushes.Yellow;
-
-            button1.Content = text1;
             button1.Width = 150;
-            button1.Height = 90;
+            button1.Height = 150;
+            button1.Click += (s, e) => button1.Content = text1;
 
-            button2.Content = text2;
             button2.Width = 150;
-            button2.Height = 90;
+            button2.Height = 150;
+            button2.Click += (s, e) => button2.Content = text1;
 
-            button3.Content = text3;
             button3.Width = 150;
-            button3.Height = 90;
+            button3.Height = 150;
+
+            button4.Width = 150;
+            button4.Height = 150;
+
+            button5.Width = 150;
+            button5.Height = 150;
+
+            button6.Width = 150;
+            button6.Height = 150;
+
+            button7.Width = 150;
+            button7.Height = 150;
+
+            button8.Width = 150;
+            button8.Height = 150;
+
+            button9.Width = 150;
+            button9.Height = 150;
+
 
         }
     }
